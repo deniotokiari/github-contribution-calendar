7f049464fbc559da0dc5560e9bf8665f03a577b4
deniotokiari <deniotokiari@gmail.com>
2023-10-27 11:50:11 +0200

    fix ad on config screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "pl.deniotokiari.githubcontributioncalendar"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/pl/deniotokiari/githubcontributioncalendar/activity/AppWidgetConfigurationActivity.kt
+++ app/src/main/java/pl/deniotokiari/githubcontributioncalendar/activity/AppWidgetConfigurationActivity.kt
@@ -16,7 +16,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -87,7 +87,7 @@ class AppWidgetConfigurationActivity : ComponentActivity(), KoinComponent {
     @Composable
     private fun MainContent() {
         GitHubContributionCalendarTheme {
-            Surface(modifier = Modifier.wrapContentSize(), color = MaterialTheme.colorScheme.background) {
+            Surface(modifier = Modifier.wrapContentHeight().fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
                 var username by remember { mutableStateOf("") }
 
                 Column(
