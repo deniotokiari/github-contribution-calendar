571f8fd6aca91c702709e5090f77ac1c4f1d8120
deniotokiari <deniotokiari@gmail.com>
2024-11-12 17:19:55 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = appId
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.5"
+        versionCode = 12
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
